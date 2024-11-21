97bdee0bcdb6495df7fdae8d8098188af87aa09a
Aimararivv <xitziafierro@gmail.com>
2024-11-20 20:27:19 -0700

    no me deja actualizar repos y por eso hago nuevos P.3.9

---
+++ libreriaXAGFRRR/Data/Services/AuthorsService.cs
@@ -0,0 +1,36 @@
+﻿using libreriaXAGFRRR.Data.Models;
+using libreriaXAGFRRR.Data.ViewModels;
+using System;
+using System.Linq;
+
+namespace libreriaXAGFRRR.Data.Services
+{
+    public class AuthorsService
+    {
+        private AppDbContext _context;
+        public AuthorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+        //Metodo que nos permite agregar un nuevo autor en la BD
+        public void AddAuthor(AuthorVM author)
+        {
+            var _author = new Author()
+            {
+                FullName = author.FullName
+      
+            };
+            _context.Authors.Add(_author);
+            _context.SaveChanges();
+        }
+        public AuthorWithBooksVM GetAuthorWithBooks(int authorId)
+        {
+            var _author = _context.Authors.Where(n => n.Id == authorId).Select(n => new AuthorWithBooksVM ()
+            {
+                FullName = n.FullName,
+                BookTitles = n.book_Authors.Select(n => n.Book.Titulo).ToList()
+            }).FirstOrDefault();
+            return _author;
+        }
+    }
+}
---
+++ libreriaXAGFRRR/Data/Services/PublishersService.cs
@@ -0,0 +1,52 @@
+﻿using libreriaXAGFRRR.Data.Models;
+using libreriaXAGFRRR.Data.ViewModels;
+using System;
+using System.Linq;
+
+namespace libreriaXAGFRRR.Data.Services
+{
+    public class PublishersService
+    {
+        private AppDbContext _context;
+        public PublishersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        //Metodo que nos permite agregar una nueva editora en la BD
+        public void AddPublisher(PublisherVM publisher)
+        {
+            var _publisher = new Publisher()
+            {
+                Name = publisher.Name
+
+            };
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+        }
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _publisherData = _context.Publishers.Where(n => n.Id == publisherId)
+                .Select(n => new PublisherWithBooksAndAuthorsVM()
+                {
+                    Name = n.Name,
+                    BookAuthors = n.Books.Select(n => new BookAuthorVM()
+                    {
+                        BookName = n.Titulo,
+                        BookAuthors = n.book_Authors.Select(n => n.Author.FullName).ToList()
+                    }).ToList()
+                }).FirstOrDefault();
+            return _publisherData;
+        }
+
+        internal void DeletePublisherById(int id)
+        {
+            var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == id);
+            if (_publisher != null)
+            {
+                _context.Publishers.Remove(_publisher);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
